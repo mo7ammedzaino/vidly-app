be4290dd60bed551ee7fb9b22df98e6a664fe2bb
Mohammed Zaino <mohdzaino2001@gmail.com>
2023-02-10 23:35:05 +0200

    all file

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from "react";
 import { getMovies } from "./../services/fakeMovieService";
+import Like from "./common/like";
 class Movies extends Component {
   state = {
     movies: getMovies(),
   };
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
   render() {
@@ -22,6 +30,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -31,6 +40,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
